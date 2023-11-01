7615af832570bf235d412bd5cb5bc3f4984ec28a
AshkanArabim <ashkan.arabim@gmail.com>
2023-11-01 14:29:16 -0600

    add redirects plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,19 +2,20 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  siteMetadata: {
-    title: `blog`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: [
-    "gatsby-plugin-mdx", 
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `content`,
-        path: `${__dirname}/src/content`,
-      },
-    },
-    "gatsby-transformer-remark",
-  ],
+	siteMetadata: {
+		title: `blog`,
+		siteUrl: `https://www.yourdomain.tld`,
+	},
+	plugins: [
+		"gatsby-plugin-mdx",
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `content`,
+				path: `${__dirname}/src/content`,
+			},
+		},
+		"gatsby-transformer-remark",
+		"gatsby-plugin-netlify-redirect",
+	],
 };
